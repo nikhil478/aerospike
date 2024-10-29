3196a59cf0e80aa611621f471b9536d28fa37778
NIKHIL MATTA <nikhilmatta45@gmail.com>
2024-10-29 12:36:26 +0000

    test connection and read , write a record successfully

--- go.mod
+++ go.mod
@@ -4,12 +4,10 @@ go 1.22
 
 toolchain go1.23.1
 
+require github.com/aerospike/aerospike-client-go/v7 v7.6.0
+
 require (
-	github.com/aerospike/aerospike-client-go/v7 v7.6.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/nxadm/tail v1.4.8 // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/ginkgo/v2 v2.20.1 // indirect
 	github.com/onsi/gomega v1.34.2 // indirect
 	github.com/yuin/gopher-lua v1.1.1 // indirect
 	golang.org/x/net v0.30.0 // indirect
@@ -19,6 +17,4 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241021214115-324edc3d5d38 // indirect
 	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
-	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,4 +1,69 @@
-package main 
+package main
 
+import (
+    "log"
+    "time"
 
-func main(){}
+    "github.com/aerospike/aerospike-client-go/v7"
+)
+
+func main() {
+    // ***
+    // Setup
+    // ***
+
+    address := "127.0.0.1"      // Aerospike Cloud cluster address
+    port := 3000                // Aerospike Cloud cluster port
+    namespace := "test"         // Cluster namespace
+    set := "foo"                // Set name within namespace
+
+    // Create the client and connect to the database
+    client, err := aerospike.NewClient(address, port)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Close()
+
+    // ***
+    // Write a record
+    // ***
+
+    // Create a WritePolicy to set the TotalTimeout for writes
+    // default 1000 ms
+    writePolicy := aerospike.NewWritePolicy(0, 0)
+    writePolicy.TotalTimeout = 5000 * time.Millisecond
+
+    // Create the record key
+    // A tuple consisting of namespace, set name, and user defined key
+    key, err := aerospike.NewKey(namespace, set, "bar")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Create a bin to store data within the new record
+    bin := aerospike.NewBin("myBin", "Hello World!")
+
+    //Write the record to your database
+    err = client.PutBins(writePolicy, key, bin)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println("Succesfully wrote record")
+
+    // ***
+    // Read back the record we just wrote
+    // ***
+
+    // Create a Policy to set the TotalTimeout for reads
+    // default 1000 ms
+    readPolicy := aerospike.NewPolicy()
+    readPolicy.TotalTimeout = 5000 * time.Millisecond
+
+    // Read the record
+    record, err := client.Get(readPolicy, key)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Printf("Record: %s", record.Bins)
+}
